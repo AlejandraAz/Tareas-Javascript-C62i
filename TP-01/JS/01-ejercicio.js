6280f29228bf5d9a74349dce88b414d9d62e7e07
Alejandra Alaniz <dominguezale377@gmail.com>
2023-06-28 22:04:57 -0300

    Iniciando con la resolución de ejercicios

---
+++ TP-01/JS/01-ejercicio.js
@@ -0,0 +1,2 @@
+//1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+alert("Bienvenido/a a Rolling Code 😊 ✨!");
---
+++ TP-01/JS/02-ejercicio.js
@@ -0,0 +1,2 @@
+//  Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+document.write("¡Hello World!!🗺️")
