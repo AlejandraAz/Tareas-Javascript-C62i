8aefb1e7352c903ce6517512f8336d1470f8e8e4
Alejandra Alaniz <dominguezale377@gmail.com>
2023-07-14 00:26:46 -0300

    ejercicio finalizado

--- TP-01/JS/09-ejercicio.js
+++ TP-01/JS/09-ejercicio.js
@@ -5,4 +5,18 @@ input: Hola mundo
 Output: oauo */
 
 
+let frase = prompt("Ingrese una frase:"); 
 
+let vocales = ""; 
+
+if(!isNaN(frase)){
+    console.error('Sólo debes ingresar palabras..')
+}else{
+    for (let i = 0; i < frase.length; i++) {
+        let letra = frase.charAt(i).toLowerCase(); 
+        if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u") {
+            vocales += letra; 
+        }
+        }
+        console.log("Vocales encontradas: " + vocales); 
+}
