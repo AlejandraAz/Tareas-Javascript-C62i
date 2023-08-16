116e0599eec81534bab562543fa0c6322c3ce0da
Alejandra Alaniz <dominguezale377@gmail.com>
2023-08-16 11:59:17 -0300

    ejercicio 9 finalizado

--- TP-06-DOM-BOM/JS/ejercicio-09.js
+++ TP-06-DOM-BOM/JS/ejercicio-09.js
@@ -4,3 +4,45 @@
 modal.
 - Permite al usuario navegar entre las imágenes utilizando botones "Anterior"
 y "Siguiente" en el cuadro modal */
+const myModal = new bootstrap.Modal(document.getElementById('myModal'));
+let contenedor = document.getElementById("contenedor-imgs");
+let images = document.querySelectorAll('img');
+let modalImg = document.querySelector('#img-modal');
+let anterior = document.getElementById('anterior');
+let siguiente = document.getElementById('siguiente');
+let cerrar = document.getElementById('boton_cierre');
+let modal = document.querySelector('#myModal');
+let inicioImg = 0;
+
+
+const clickImg = (index)=>{
+    modalImg.src = images[index].src;
+    inicioImg = index;
+    myModal.show(index)
+}
+
+images.forEach(function(image, index) {
+    image.addEventListener('click', function() {
+        modalImg.src = images[index].src;
+        myModal.show()
+    });
+});
+
+const anteriorImg = ()=>{
+    inicioImg--;
+    if (inicioImg < 0) {
+        inicioImg = images.length - 1;
+        }
+        modalImg.src = images[inicioImg].src;
+}
+
+const siguienteImg = ()=>{
+    inicioImg++;
+    if (inicioImg >= images.length) {
+        inicioImg = 0;
+        }
+        modalImg.src = images[inicioImg].src;
+}
+
+    
+
